4a70c5e680953eabc27607de3df3cd06b2296340
EduPassero <eduardopassero@hotmail.com>
2023-04-05 15:51:33 -0300

    Quarta feira

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var posicaoAntiga = window.pageYOffset;
+
+window.onscroll = function(){
+  var posicaoatual = window.pageYOffset;
+  if(posicaoAntiga > posicaoatual){
+    document.getElementById('nav').classList.add('activeNav')
+    document.getElementById("nav").style.top = "0";
+    if(posicaoatual == 0){
+      document.getElementById('nav').classList.remove('activeNav')
+    }
+  }
+  else{
+    //document.getElementById('nav').classList.remove('activeNav')
+    document.getElementById("nav").style.top = "-100px";
+  }
+  posicaoAntiga = posicaoatual;
+}
+
+
 const $carousel3 = document.querySelector(".js_carousel_clients");
 new Glider($carousel3, {
   slidesToShow: 1,
